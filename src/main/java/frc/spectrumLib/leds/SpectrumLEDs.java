2942a75190cbef28a72c387ea41d5972dae278ef
Spectrum3847 <spectrum3847@gmail.com>
2024-09-18 16:04:47 -0500

    LED Changes
    Will be transferred to a branch off of ElevatorSIm

--- src/main/java/frc/spectrumLib/leds/SpectrumLEDs.java
+++ src/main/java/frc/spectrumLib/leds/SpectrumLEDs.java
@@ -1,14 +1,16 @@
 package frc.spectrumLib.leds;
 
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SpectrumLEDs extends SubsystemBase {
     protected int priority = 0;
     // LED IO
+    byte[] priorityBuffer;
     private AddressableLED leds;
-    private SpectrumLEDBuffer buffer;
+    private AddressableLEDBuffer buffer;
     protected Runnable defaultPattern = () -> {}; // Start with blank pattern
     private boolean update = true;
     private int counter = 0;
@@ -19,8 +21,9 @@ public class SpectrumLEDs extends SubsystemBase {
         leds = new AddressableLED(port);
         this.length = length;
         leds.setLength(length);
-        buffer = new SpectrumLEDBuffer(length);
-        leds.setData(buffer.getLEDBuffer());
+        buffer = new AddressableLEDBuffer(length);
+        priorityBuffer = new byte[length];
+        leds.setData(buffer);
         leds.start();
     }
 
@@ -33,13 +36,13 @@ public class SpectrumLEDs extends SubsystemBase {
         leds.close();
         leds = new AddressableLED(port);
         leds.setLength(length);
-        buffer = new SpectrumLEDBuffer(length);
+        buffer = new AddressableLEDBuffer(length);
         leds.start();
     }
 
     public void periodic() {
         // Have our LEDs calculate only every other cycle
-        leds.setData(buffer.getLEDBuffer());
+        leds.setData(buffer);
 
         if (counter % 5 == 0) {
             time = counter * 0.02;
@@ -56,7 +59,10 @@ public class SpectrumLEDs extends SubsystemBase {
     }
 
     public void resetPriority() {
-        buffer.resetPriorityBuffer();
+        for (int i = 0; i < priorityBuffer.length; i++) {
+            priorityBuffer[i] = 0;
+            buffer.setRGB(i, 0, 0, 0);
+        }
     }
 
     public boolean getUpdate() {
@@ -68,17 +74,23 @@ public class SpectrumLEDs extends SubsystemBase {
     }
 
     public void setLED(int i, Color c, int priority) {
-        buffer.setLED(i, c, priority);
-        buffer.setLED(i + length / 2, c, priority);
+        buffer.setLED(i, c);
+        priorityBuffer[i] = (byte) priority;
+        buffer.setLED(i + length / 2, c);
+        priorityBuffer[i + length / 2] = (byte) priority;
     }
 
     public void setLED(int i, int r, int g, int b, int priority) {
-        buffer.setLED(i, new Color(r, g, b), priority);
-        buffer.setLED(i + length / 2, new Color(r, g, b), priority);
+        buffer.setLED(i, new Color(r, g, b));
+        priorityBuffer[i] = (byte) priority;
+        buffer.setLED(i + length / 2, new Color(r, g, b));
+        priorityBuffer[i + length / 2] = (byte) priority;
     }
 
     public void setHSV(int i, int h, int s, int v, int priority) {
-        buffer.setHSV(i, h, s, v, priority);
-        buffer.setHSV(i + length / 2, h, s, v, priority);
+        buffer.setHSV(i, h, s, v);
+        priorityBuffer[i] = (byte) priority;
+        buffer.setHSV(i + length / 2, h, s, v);
+        priorityBuffer[i + length / 2] = (byte) priority;
     }
 }
