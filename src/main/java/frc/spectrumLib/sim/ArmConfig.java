5ade981beb6b384ea1350875546111d0c1574d4b
Allen Gregory <allengregoryiv@gmail.com>
2024-10-10 17:12:36 -0500

    slightly working arm sim

--- src/main/java/frc/spectrumLib/sim/ArmConfig.java
+++ src/main/java/frc/spectrumLib/sim/ArmConfig.java
@@ -11,7 +11,7 @@ public class ArmConfig {
     @Getter @Setter private double ratio = 50;
     @Getter @Setter private double length = 0.5;
     @Getter @Setter private double simMOI = 1.2;
-    @Getter @Setter private double simCGLength = 0.3;
+    @Getter @Setter private double simCGLength = 0.2;
     @Getter @Setter private double minAngle = Math.toRadians(-60);
     @Getter @Setter private double maxAngle = Math.toRadians(90);
     @Getter @Setter private double startingAngle = Math.toRadians(90);
@@ -22,14 +22,14 @@ public class ArmConfig {
             double pivotY,
             double ratio,
             double length,
-            double minAngle,
-            double maxAngle,
-            double startingAngle) {
+            double minAngleDegrees,
+            double maxAngleDegrees,
+            double startingAngleDegrees) {
         this.ratio = ratio;
         this.length = length;
-        this.minAngle = Math.toRadians(minAngle);
-        this.maxAngle = Math.toRadians(maxAngle);
-        this.startingAngle = Math.toRadians(startingAngle);
+        this.minAngle = Math.toRadians(minAngleDegrees);
+        this.maxAngle = Math.toRadians(maxAngleDegrees);
+        this.startingAngle = Math.toRadians(startingAngleDegrees);
         this.pivotX = pivotX;
         this.pivotY = pivotY;
     }
